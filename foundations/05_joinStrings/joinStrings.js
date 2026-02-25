e614da0d32991f9be1df84435d47d070b776e10e
Nadol <nadinelucanas@gmail.com>
2026-02-25 14:25:14 +0800

    add answers to js exercises 1 to 4

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,9 +9,16 @@
 
 // Add your code right below, good luck!
 
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = "Carlos Stevenson";
+const age = 18;
 
+const greeting = "Hello! My name is " + fullName + ". " + "My age is " + age + ". ";
 
-
+console.log(greeting);
 
 // Do not change this
 module.exports = {
